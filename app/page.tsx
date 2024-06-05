75ed2c1ac31aea205422b20090534f55e9d7c190
Vittalds25 <vittalds25@gmail.com>
2024-06-05 13:41:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,7 @@ export default function Home() {
       
           <code className="font-mono font-bold"> 1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each.</code>
         </p>
-       <code className="font-mono font-bold">Ans: 
-5 most favourite – 
-1.Respond to 25+ support requests via email every day.- I love to get to know issues and their root causes, if we have more tickets the probability of getting new issues are more and that would help me gain more knowledge and skills.
-2.Dig through logs to troubleshoot a customer's broken project. - I always prefer to fix the issue myself by going through the logs and knowing what might have caused the issue, for instance if the user has made some mistakes that led to and error we would easily get those on logs and update the user on the same.
-3.Write and maintain support articles and docs pages. - This is a very important and useful task which would reduce our burden in training the new recruits or adding this as a self assist document for the users.
-4.Analyze hundreds of support tickets to spot trends the product team can use - This is a very helpful task that would help us in reducing the tickets flow. If we are getting similar tickets that means the product is not built in a user friendly way and we would have to get this to product team and have them work on it.
-5.Work with engineering teams during incidents and provide updates to internal and external stakeholders. - This is very important task as the user will be waiting for an update, imagining myself in the place of the user I would want some solution right away and if I get to connect with the right person then I would be able to help the user ASAP.
-</code>
-        </p>
+      
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
